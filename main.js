60c9a05be7f9d56d35c41d27905e772ed7bfcff4
Charles-dav <kelliviscent1@gmail.com>
2020-10-17 23:47:37 +0100

    add new updates and changes

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
+const display1 = document.querySelector('.kilo')
+const display2 = document.querySelector('.ounce')
 
 function sumVal(){
     const calC = document.formCal.textview1.value
     document.querySelector('#outputKilogram').innerHTML = calC / 1000
     document.querySelector('#ounces').textContent = calC / 0.035274
+    display1.classList.add('display');
+    display2.classList.add('display');
 }
 
 function clearScreen(){
-    document.querySelector('#outputKilogram').innerHTML = ""
-    document.querySelector('#ounces').textContent = ""
-    document.formCal.textview1.value = ''
+    document.querySelector('#outputKilogram').innerHTML = "";
+    document.querySelector('#ounces').textContent = "";
+    document.formCal.textview1.value = '';
+    display1.classList.remove('display');
+    display2.classList.remove('display');
 }
+
